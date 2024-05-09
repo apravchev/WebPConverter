5f3ca9c7ae52809c610f7c5ec25762d65db2dcd9
alexeod <90037308+apravchev@users.noreply.github.com>
2024-05-09 16:26:06 +0300

    optimised associations func for readability.

--- backend/src/services/database/DBRelations.js
+++ backend/src/services/database/DBRelations.js
@@ -4,8 +4,8 @@ class DatabaseRelationsService {
     this.models = models;
   }
   createOneToMany(one, many) {
-    many.belongsTo(one);
     one.hasMany(many);
+    many.belongsTo(one);
   }
   createOneToOne(one, two) {
     one.hasOne(two);
@@ -16,14 +16,13 @@ class DatabaseRelationsService {
       this.models;
     this.createOneToMany(FileFormat, FileInfo);
     this.createOneToMany(FileFormat, ConversionConfiguration);
+    this.createOneToMany(ConversionConfiguration, ConversionInfo);
     FileInfo.belongsToMany(FileInfo, {
       through: ConversionInfo,
       otherKey: "converted_id",
       foreignKey: "original_id",
       as: "converted_id",
     });
-    ConversionConfiguration.hasMany(ConversionInfo);
-    ConversionInfo.belongsTo(ConversionConfiguration);
   }
 }
 module.exports = DatabaseRelationsService;
